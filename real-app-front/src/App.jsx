79a36a0d87eb512cd69b076a77236576de3fa8cd
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2023-06-16 16:49:41 +0300

    delete css. test bootstrap

---
+++ real-app-front/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-danger">Hello</div>
+      <i className="bi bi-person"></i>
+    </div>
+  );
+}
+
+export default App;
