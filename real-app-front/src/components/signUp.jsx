ed401fadc77ee4a71c17e2e43ac7f39d9b0c5af9
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2023-06-17 14:30:56 +0300

    sighnUp form

--- real-app-front/src/components/signUp.jsx
+++ real-app-front/src/components/signUp.jsx
@@ -1,14 +1,33 @@
+import { Form } from "react-router-dom";
 import PageHeader from "./common/pageHeader";
 const SignUp = () => {
   return (
-    <PageHeader
-      title={
-        <>
-          Sign Up <i className="bi bi-boxes"></i> to Real App
-        </>
-      }
-      description={"Create a new account, it is free"}
-    />
+    <>
+      <PageHeader
+        title={
+          <>
+            Sign Up <i className="bi bi-boxes"></i> to Real App
+          </>
+        }
+        description={"Create a new account, it is free"}
+      />
+
+      <form>
+        <div class="form-group my-1">
+          <label for="email" class="form-label">
+            Email
+            <span className="text-danger"> *</span>
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="form-control is-invalid"
+          />
+          <span className="invalid-feedback">some error</span>
+        </div>
+      </form>
+    </>
   );
 };
 
