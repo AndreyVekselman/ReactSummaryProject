caeb688088ed1e2ffbad0dfcaa293eefd774a39f
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2023-06-17 16:19:01 +0300

    add validation to sightUp form

--- real-app-front/src/components/signUp.jsx
+++ real-app-front/src/components/signUp.jsx
@@ -1,6 +1,37 @@
 import PageHeader from "./common/pageHeader";
 import Input from "./common/input";
+import { useFormik } from "formik";
+
 const SignUp = () => {
+  const form = useFormik({
+    validateOnMount: true,
+    initialValues: {
+      email: "",
+      password: "",
+      name: "",
+    },
+    validate({ email, password, name }) {
+      const errors = {};
+      let isValid = true;
+      if (!email) {
+        errors.email = "email field is required";
+        isValid = false;
+      }
+      if (!password) {
+        errors.password = "password field is required";
+        isValid = false;
+      }
+      if (!name) {
+        errors.name = "name field is required";
+        isValid = false;
+      }
+      return isValid ? null : errors;
+    },
+    onSubmit(values) {
+      console.log("submited", values);
+    },
+  });
+
   return (
     <>
       <PageHeader
@@ -12,10 +43,37 @@ const SignUp = () => {
         description={"Create a new account, it is free"}
       />
 
-      <form>
-        <Input label="Email" name="email" type="email" required />
-        <Input label="Password" name="password" type="password" required />
-        <Input label="Name" name="name" type="text" required />
+      <form onSubmit={form.handleSubmit} noValidate>
+        <Input
+          {...form.getFieldProps("email")}
+          label="Email"
+          type="email"
+          required
+          error={form.touched.email && form.errors.email}
+        />
+        <Input
+          {...form.getFieldProps("password")}
+          label="Password"
+          type="password"
+          required
+          error={form.touched.password && form.errors.password}
+        />
+        <Input
+          {...form.getFieldProps("name")}
+          label="Name"
+          type="text"
+          required
+          error={form.touched.name && form.errors.name}
+        />
+        <div className="my-2">
+          <button
+            type="submit"
+            disabled={!form.isValid}
+            className="btn btn-primary"
+          >
+            Sign UP
+          </button>
+        </div>
       </form>
     </>
   );
