e943db7440d19d2070700117f4ff4fc7ae2cba57
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2023-06-24 14:00:46 +0300

    update protected route

--- real-app-front/src/components/common/protectedRoute.jsx
+++ real-app-front/src/components/common/protectedRoute.jsx
@@ -1,5 +1,13 @@
-const ProtectedRoute = () => {
-  return <></>;
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/auth.context";
+
+const ProtectedRoute = ({ children }) => {
+  const { user } = useAuth();
+
+  if (!user) {
+    return <Navigate to="/sign-in" />;
+  }
+  return children;
 };
 
 export default ProtectedRoute;
