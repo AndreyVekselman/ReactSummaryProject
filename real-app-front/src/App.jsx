cf11e14d97c296d8456160f6d176437817311488
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2023-06-24 17:27:24 +0300

    connect toastcontainer to app

--- real-app-front/src/App.jsx
+++ real-app-front/src/App.jsx
@@ -10,13 +10,13 @@ import SignOut from "./components/signOut";
 import SignUpBiz from "./components/signUpBiz";
 import BizCards from "./components/bizCards";
 import ProtectedRoute from "./components/common/protectedRoute";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App d-flex flex-column min-vh-100 ">
+      <ToastContainer />
       <header>
         <Navbar />
       </header>
