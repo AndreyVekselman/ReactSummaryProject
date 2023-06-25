6f427a9d317a6a4d98d390f9e88530102ebc73e9
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2023-06-25 09:44:55 +0300

    update cardDelete

--- real-app-front/src/components/cardDelete.jsx
+++ real-app-front/src/components/cardDelete.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import cardsService from "../services/cardService";
+
 const CardDelete = () => {
+  const navigate = useNavigate();
+  const id = useParams();
+  useEffect(() => {
+    const deleteCard = async () => {
+      await cardsService.deleteCard(id);
+      navigate("/bizCards");
+    };
+    deleteCard();
+  }, [id, navigate]);
   return null;
 };
 
